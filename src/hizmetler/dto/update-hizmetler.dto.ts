ef30226a625f023a4f377265e2467cb797c9f030
Barış Gül <89139855+barissgul@users.noreply.github.com>
2025-10-19 13:23:17 +0300

    Create update-hizmetler.dto.ts

---
+++ src/hizmetler/dto/update-hizmetler.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from "@nestjs/swagger";
+import { CreateHizmetlerDto } from "./create-hizmetler.dto";
+
+export class UpdateHizmetlerDto extends PartialType(CreateHizmetlerDto) {}
